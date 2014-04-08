7004b5d7bd684cae6f5501bc0f030bc2433540c7
Patrick Serrano <hello@patrickserrano.com>
2014-04-07 22:57:41 -0400

    Quick update to license text

--- WPKN-Live/PlayerViewController.h
+++ WPKN-Live/PlayerViewController.h
@@ -7,6 +7,8 @@
 //
 //  The MIT License (MIT)
 //
+//  Copyright (c) 2013-2014 Patrick Serrano.
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
@@ -24,7 +26,6 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
-
 
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
